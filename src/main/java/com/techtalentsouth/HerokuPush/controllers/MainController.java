7967ccae5756448abd35bd8f7b64f4cd8b20339f
codyg7 <45825659+codyg7@users.noreply.github.com>
2019-02-06 10:14:14 -0500

    Heroku Push take 2

--- src/main/java/com/techtalentsouth/HerokuPush/controllers/MainController.java
+++ src/main/java/com/techtalentsouth/HerokuPush/controllers/MainController.java
@@ -2,19 +2,15 @@ package com.techtalentsouth.HerokuPush.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@RestController
 public class MainController {
-	
-	public MainController() {
-		
-	}
-	
-	@GetMapping("/")
-	public ModelAndView indexMethod() {
-		ModelAndView mv = new ModelAndView("index");
-		return mv;
-	}
-
+    
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
 }
