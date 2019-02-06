13080db7d2ab13688aaf73fffea67cf1d7be1205
codyg7 <45825659+codyg7@users.noreply.github.com>
2019-02-06 09:59:07 -0500

    Heroku Push

---
+++ src/main/java/com/techtalentsouth/HerokuPush/HerokuPushApplication.java
@@ -0,0 +1,14 @@
+package com.techtalentsouth.HerokuPush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuPushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuPushApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/techtalentsouth/HerokuPush/HerokuPushApplicationTests.java
@@ -0,0 +1,17 @@
+package com.techtalentsouth.HerokuPush;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HerokuPushApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
